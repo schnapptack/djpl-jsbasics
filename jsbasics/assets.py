15e2f9fdfadf24b6f9f65f89474b727c971ae1cc
Toni Michel <toni.michel@schnapptack.de>
2016-04-26 10:41:05 +0200

    added jqmobile swipe detection

--- jsbasics/assets.py
+++ jsbasics/assets.py
@@ -5,6 +5,7 @@ def refine_STATICS(original):
         'jquery/js/jquery-1.9.1.js',
         'jquery/js/jquery.migrate.1.3.0.js',
         'jquery/js/jquery-ui-1.10.1.js',
+        'jquery/js/jquery.mobile.custom.min.js',
         'jquery/js/django-csrf.js',
         'underscore-min.js',
     ]
