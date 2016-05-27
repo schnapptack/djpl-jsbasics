b227177ca56fc486e6c12144a1b3ee9febab35e2
michael <michael@samaritan.fritz.box>
2016-05-27 14:45:25 +0200

    updateded js basics

--- jsbasics/assets.py
+++ jsbasics/assets.py
@@ -2,9 +2,9 @@
 
 def refine_STATICS(original):
     original['PUBLIC']['js']['files'] += [
-        'jquery/js/jquery-1.9.1.js',
-        'jquery/js/jquery.migrate.1.3.0.js',
-        'jquery/js/jquery-ui-1.10.1.js',
+        'jquery/js/jquery-1.12.4.min.js',
+        'jquery/js/jquery.migrate.1.4.1.min.js',
+        'jquery/js/jquery-ui.1.11.4.min.js',
         'jquery/js/jquery.mobile.custom.min.js',
         'jquery/js/django-csrf.js',
         'underscore-min.js',
